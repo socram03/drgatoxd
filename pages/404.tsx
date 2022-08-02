97dcf7f7348cf5e6a22d632d542953e824e967f8
ItzDrGato <mathisebas18@gmail.com>
2022-08-02 01:18:01 -0500

    fix(pages/404.tsx): do redirect with useEffect

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,14 @@
-import { GetStaticProps } from 'next';
+import { Flex, Spinner } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-export const getStaticProps: GetStaticProps = async () => {
-	return {
-		redirect: {
-			destination: '/',
-			permanent: true
-		}
-	};
-};
+export default function Error() {
+	const router = useRouter();
+	useEffect(() => void setTimeout(() => router.push('/'), 1500), [router]);
 
-export default getStaticProps;
+	return (
+		<Flex gap={5} justify="center" align="center" w="100%" h="100vh">
+			<Spinner size="md" /> Redirigiendo...
+		</Flex>
+	);
+}
